73390d0c96ab4db3b50922e1eadc7d2421b7f5de
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-28 21:50:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join
-  elsif array.length == 2 
+  elsif array.length == 2
     array.join(" and ")
+  else array.length => 3
+    
   end
 end
